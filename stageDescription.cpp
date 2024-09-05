9170c11c4c28b5c0ec426b8a932c701b9b507bb6
TFAURE <thomas.fe2005@outlook.com>
2024-09-05 11:36:45 +0200

    ajout in main création mission
    modification du main pour dréer une mission avec des cin

--- stageDescription.cpp
+++ stageDescription.cpp
@@ -2,10 +2,38 @@
 //
 
 #include <iostream>
+#include "Mission.h"
+#include "Entreprise.h"
+using namespace std;
 
 int main()
 {
-    std::cout << "Salut comment ca va ?\n";
+    // Création d'une mission en demandant les informations à l'utilisateur
+    string descriptif;
+    int nbPersonne, nbHeure;
+
+    cout << "Entrez le descriptif de la mission : ";
+    getline(cin, descriptif);
+
+    cout << "Entrez le nombre de personnes requises : ";
+    cin >> nbPersonne;
+
+    cout << "Entrez le nombre d'heures estimees : ";
+    cin >> nbHeure;
+
+    // Création de la mission avec les données saisies
+    Mission mission(nbPersonne, descriptif, nbHeure);
+
+    // Affichage des informations de la mission
+    cout << "\nMission creee :" << endl;
+    cout << "Descriptif : " << mission.getDescriptifMission() << endl;
+    cout << "Nombre de personnes : " << mission.getNbPersonne() << endl;
+    cout << "Nombre d'heures estimées : " << mission.getNbHeure() << endl;
+
+    // Exemple d'utilisation d'un setter pour modifier une information
+    mission.setNbPersonne(mission.getNbPersonne() + 2);
+    cout << "Nombre de personnes mis à jour : " << mission.getNbPersonne() << endl;
+
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
